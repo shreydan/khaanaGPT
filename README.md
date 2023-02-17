f16db58385bf29132ab4cb2ec51db0a9ed93ed69
shreydan <iamshreydan@gmail.com>
2023-02-17 23:25:33 +0530

    remove streamlit

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Just a basic app to generate Indian recipes from listed ingredients. It's good, not great, it might add more ingredients or ignore some existing ingredients.
 
 This was my first attempt at fine-tuning causalLM models. I used `GPT-2 Small` for this and it was fun, learnt a lot and the outputs are as expected.
-
-Example:
-
-![example generated image](https://i.imgur.com/lJJwC4V.png)
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from transformers import pipeline
 
 model_path = './khaanaGPT'
@@ -9,27 +8,15 @@ def create_prompt(ingredients):
     s = f"<|startoftext|>Ingredients:\n{ingredients}\n\nInstructions:\n"
     return s
 
-
-model = pipeline(task='text-generation',model=model_path)
-
-st.title('खानाGPT')
-st.caption("it's not perfect, might ± ingredients, it's just a fun project to showcase fine-tuning a causal language model like GPT-2")
-
-with st.container():
-    st.subheader('Ingredients')
-    text = st.text_input('separate ingredients with a comma.',placeholder='e.g. chicken, tomatoes, red bell peppers, ...')
-    text = text.strip()
-    submitted = st.button('Create Recipe',type='primary')
-
-with st.container():
-    if len(text)!=0 or submitted:
-        prompt = create_prompt(text)
-        with st.spinner('cooking something for you...'):
-            recipe = model(prompt,
+def generate(prompt):
+    recipe = model(prompt,
                     max_new_tokens=512,
                     penalty_alpha=0.5,
                     top_k=5,
                     pad_token_id=50259,
                     )[0]['generated_text']
-            recipe = recipe.replace('<|startoftext|>','')
-        st.write(recipe)
+    recipe = recipe.replace('<|startoftext|>','')
+    return recipe
+
+
+model = pipeline(task='text-generation',model=model_path)

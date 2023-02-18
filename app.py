b9792e2630c3d6a1679bc4929a43f6a18f250aba
shreydan <iamshreydan@gmail.com>
2023-02-18 13:26:46 +0530

    add titles

--- app.py
+++ app.py
@@ -31,16 +31,29 @@ def wrapper(ingredients):
     recipe = generate(prompt)
     return recipe
 
+intro_html = """
+<center><h1>खानाGPT</h1></center>
+<center>
+<p>it's not perfect, may ± ingredients. The recipes are coherent, 
+but the main purpose of this project was to understand fine-tuning a causalLM like GPT-2.
+This model was fine-tuned on GPT-2 Small.</p>
+</center>
+"""
 
 with gr.Blocks() as demo:
-    gr.HTML('<center><h1>KhaanaGPT</h1></center>')
-    ingredients = gr.Textbox(label="ingredients",placeholder='separate the ingredients with a comma.')
+    gr.HTML(intro_html)
+
+    ingredients = gr.Textbox(label="ingredients",
+    placeholder='separate the ingredients with a comma.')
+
     output = gr.Textbox(label="recipe",lines=15,)
     greet_btn = gr.Button("Create a recipe!")
+
     gr.Examples(['yellow dal, turmeric, green peas, tomatoes',
                 'chicken, soy sauce, tomato sauce, vinegar'],
                 inputs=ingredients
             )
+
     greet_btn.click(fn=wrapper, inputs=ingredients, outputs=output)
 
 demo.launch()
